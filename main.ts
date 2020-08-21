a72755fe650e74da710a0dc01cb6d16dbbe0a647
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-22 03:02:26 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ namespace task {
     //% blockId="TaskStart" block="start task"
     //% blockGap=0 weight=0
     export function TaskStart(): void {
-        basic.showicon(HAPPY)
+        basic.showIcon(Happy)
     }
 
     //% blockId="TaskStop" block="stop task"
     //% blockGap=0 weight=0
     export function TaskStop(): void {
-        basic.showicon(SAD)
+        basic.showIcon(Sad)
     }
 }
