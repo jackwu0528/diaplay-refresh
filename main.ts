b386e7a66631ed223eb695b59e41b818263eeef7
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-22 02:39:59 +0800

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,14 @@
+//% weight=0 color=#3CB371 icon="\uf0ad" block="Task"
+namespace task {
+    //% blockId="TaskStart" block="start task"
+    //% blockGap=0 weight=0
+    export function TaskStart(): void {
+        basic.show_icon(IconNames.HAPPY)
+    }
+
+    //% blockId="TaskStop" block="stop task"
+    //% blockGap=0 weight=0
+    export function TaskStop(): void {
+        basic.show_icon(IconNames.SAD)
+    }
+}
