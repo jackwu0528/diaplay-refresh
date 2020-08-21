e27c78cf1a4c0cdbf7666480c727ddd725e78bfb
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-22 01:01:28 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ namespace task {
     //% blockId="TaskStart" block="start task"
     //% blockGap=0 weight=0
     export function TaskStart(): void {
-        microbit.enable_irq()
+        machine.enable_irq()
     }
     
     //% blockId="TaskStop" block="stop task"
     //% blockGap=0 weight=0
     export function TaskStop(): void {
-        microbit.disable_irq()
+        machine.disable_irq()
     }
 }
