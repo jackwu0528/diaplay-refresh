f5203e5fd08e2fc96a4d8f124e041eded104ec86
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-22 01:25:27 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Task"
 
-from microbit import *
-    
 namespace task {
+    from microbit import *
+        
     //% blockId="TaskStart" block="start task"
     //% blockGap=0 weight=0
     export function TaskStart(): void {
