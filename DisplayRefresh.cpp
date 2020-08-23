7e3eeadadc40f0b36e3db36eb50e1b7807139884
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-24 00:26:17 +0800

    Update DisplayRefresh.cpp

--- DisplayRefresh.cpp
+++ DisplayRefresh.cpp
@@ -2,14 +2,12 @@
 namespace DisplayRefresh {
     //% blockId="timer2_tasks_start" block="timer2 tasks start"
     //% blockGap=0 weight=0
-    //% useLoc="DisplayRefresh.Start" deprecated=1
     void timer2_tasks_start() {
         NRF_TIMER2->TASKS_START = 1;
     }
     
     //% blockId="timer2_tasks_stop" block="timer2 tasks stop"
     //% blockGap=0 weight=0
-    //% useLoc="DisplayRefresh.Stop" deprecated=1
     void timer2_tasks_stop() {
         NRF_TIMER2->TASKS_STOP = 1;
     }
