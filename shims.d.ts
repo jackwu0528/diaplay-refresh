5977bbc3ec6439e177e0433e2c8ea72ad11ab31a
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-24 02:27:16 +0800

    Create shims.d.ts

---
+++ shims.d.ts
@@ -0,0 +1,12 @@
+//% weight=0 color=#3CB371 icon="\uf0ad"
+declare namespace timer2SW {
+    //% help=timer2SW/timer2-tasks-start
+    //% weight=0
+    //% blockId=timer2_tasks_start block="timer2 tasks start"
+    function timer2_tasks_start(): void;
+    
+    //% help=timer2SW/timer2-tasks-stop
+    //% weight=0
+    //% blockId=timer2_tasks_stop block="timer2 tasks stop"
+    function timer2_tasks_stop(): void;
+}
