1efd189a58f29de32e8b6db2787be222d23513c3
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-23 20:11:40 +0800

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -1,14 +1,8 @@
-//% weight=0 color=#3CB371 icon="\uf0ad" block="Task"
 namespace task {
-    //% blockId="TaskStart" block="start task"
-    //% blockGap=0 weight=0
-    void TaskStart() {
+    void Start() {
         NRF_TIMER2->TASKS_START = 1;
     }
-
-    //% blockId="TaskStop" block="stop task"
-    //% blockGap=0 weight=0
-    void TaskStop() {
+    void Stop() {
         NRF_TIMER2->TASKS_STOP = 1;
     }
 }
