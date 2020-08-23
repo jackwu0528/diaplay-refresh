816d68dc3b6054d4a00dec7ee606cea98f96037c
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-23 23:29:33 +0800

    Update and rename task.cpp to diaplay_refresh_ON_OFF.cpp

---
+++ diaplay_refresh_ON_OFF.cpp
@@ -0,0 +1,8 @@
+namespace diaplay_refresh_ON_OFF {
+    void timer2_tasks_start() {
+        NRF_TIMER2->TASKS_START = 1;
+    }
+    void timer2_tasks_stop() {
+        NRF_TIMER2->TASKS_STOP = 1;
+    }
+}
--- task.cpp
+++
@@ -1,8 +0,0 @@
-namespace task {
-    void start() {
-        NRF_TIMER2->TASKS_START = 1;
-    }
-    void stop() {
-        NRF_TIMER2->TASKS_STOP = 1;
-    }
-}
