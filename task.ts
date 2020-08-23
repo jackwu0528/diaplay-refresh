05455fb9433cd26faf763458115db1f630227c42
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-23 20:12:44 +0800

    Update task.ts

--- task.ts
+++ task.ts
@@ -1,4 +1,14 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Task"
 namespace task {
+    //% blockId="TaskStart" block="start task"
+    //% blockGap=0 weight=0
+    void TaskStart() {
+        task.start();
+    }
 
+    //% blockId="TaskStop" block="stop task"
+    //% blockGap=0 weight=0
+    void TaskStop() {
+        task.stop();
+    }
 }
