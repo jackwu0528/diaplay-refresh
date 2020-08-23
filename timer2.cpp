c01a4a2a76b40d707a1ba9a3302fe02e0c02c5d0
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-24 02:05:47 +0800

    Update and rename timer2.cpp to timer2SW.cpp

--- timer2.cpp
+++
@@ -1,7 +0,0 @@
-void timer2_tasks_start() {
-    NRF_TIMER2->TASKS_START = 1;
-}
-
-void timer2_tasks_stop() {
-    NRF_TIMER2->TASKS_STOP = 1;
-}
---
+++ timer2SW.cpp
@@ -0,0 +1,16 @@
+//% weight=0 color=#3CB371 icon="\uf0ad"
+namespace timer2SW {
+    //% help=timer2SW/timer2-tasks-start
+    //% weight=0
+    //% blockId=timer2_tasks_start block="timer2 tasks start"
+    void timer2_tasks_start() {
+        NRF_TIMER2->TASKS_START = 1;
+    }
+
+    //% help=timer2SW/timer2-tasks-stop
+    //% weight=0
+    //% blockId=timer2_tasks_stop block="timer2 tasks stop"
+    void timer2_tasks_stop() {
+        NRF_TIMER2->TASKS_STOP = 1;
+    }
+}
