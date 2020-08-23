e6f719fd11f083f9a527c7ba4c7e51b47864aaef
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-23 19:47:40 +0800

    Update task.ts

--- task.ts
+++ task.ts
@@ -1,14 +1,4 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Task"
 namespace task {
-    //% blockId="TaskStart" block="start task"
-    //% blockGap=0 weight=0
-    export function TaskStart() : void {
-        NRF_TIMER2->TASKS_START = 1;
-    }
 
-    //% blockId="TaskStop" block="stop task"
-    //% blockGap=0 weight=0
-    export function void TaskStop() : void {
-        NRF_TIMER2->TASKS_STOP = 1;
-    }
 }
