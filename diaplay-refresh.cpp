23ec090183c95875cc804e0de5d824eef05bc8b4
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-23 23:57:11 +0800

    Update diaplay-refresh.cpp

--- diaplay-refresh.cpp
+++ diaplay-refresh.cpp
@@ -2,12 +2,14 @@
 namespace diaplay-refresh {
     //% blockId="timer2_tasks_start" block="timer2 tasks start"
     //% blockGap=0 weight=0
+    //% useLoc="diaplay-refresh.RefreshStart" deprecated=1
     void timer2_tasks_start() {
         NRF_TIMER2->TASKS_START = 1;
     }
     
     //% blockId="timer2_tasks_stop" block="timer2 tasks stop"
     //% blockGap=0 weight=0
+    //% useLoc="diaplay-refresh.RefreshStop" deprecated=1
     void timer2_tasks_stop() {
         NRF_TIMER2->TASKS_STOP = 1;
     }
