3c4937aa6311e70785f2dfa9bcd4c8e0ff536d7f
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-24 14:56:47 +0800

    Update timer2SW.cpp

--- timer2SW.cpp
+++ timer2SW.cpp
@@ -3,12 +3,12 @@ namespace timer2SW {
     //% weight=0
     //% blockId=timer2_tasks_start block="timer2 tasks start"
     void timer2_tasks_start() {
-        NRF_TIMER2->TASKS_START = 1;
+        let a = 1+2
     }
 
     //% weight=0
     //% blockId=timer2_tasks_stop block="timer2 tasks stop"
     void timer2_tasks_stop() {
-        NRF_TIMER2->TASKS_STOP = 1;
+        let a = 1*2
     }
 }
