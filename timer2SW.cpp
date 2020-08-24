bacde777ccc33921659a3a8b5306a672cc991b0f
jackwu0528 <70027123+jackwu0528@users.noreply.github.com>
2020-08-24 19:59:21 +0800

    Update timer2SW.cpp

--- timer2SW.cpp
+++ timer2SW.cpp
@@ -1,11 +1,13 @@
 //% weight=0 color=#3CB371 icon="\uf0ad"
 namespace timer2SW {
+    //% help=timer2SW/timer2-tasks-start
     //% weight=0
     //% blockId=timer2_tasks_start block="timer2 tasks start"
     void timer2_tasks_start() {
         NRF_TIMER2->TASKS_START = 1;
     }
 
+    //% help=timer2SW/timer2-tasks-stop
     //% weight=0
     //% blockId=timer2_tasks_stop block="timer2 tasks stop"
     void timer2_tasks_stop() {
